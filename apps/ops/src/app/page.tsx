eaf4f02a8a66f86cb9d465604bb3815ee7ca6a43
Shen Nan, Wong <wongshennan@gmail.com>
2026-04-11 15:39:04 +0700

    checkpoint: edit /Users/wongshennan/Documents/personal/ventures/beacon/apps/ops/src/app/page.tsx

--- apps/ops/src/app/page.tsx
+++ apps/ops/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useRef, useState } from "react";
 import { useAction, useMutation } from "convex/react";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { toast } from "sonner";
 import { api } from "../../convex/_generated/api";
