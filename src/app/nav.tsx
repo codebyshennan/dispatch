4ce069239e65f836e21c83d57829cc7e88054608
Shen Nan, Wong <wongshennan@gmail.com>
2026-04-17 02:38:28 +0700

    checkpoint: edit /Users/wongshennan/Documents/personal/ventures/dispatch/src/app/nav.tsx

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -110,8 +110,8 @@ export function NavHeader() {
 
       {/* Nav links */}
       <div style={{ display: "flex", alignItems: "center", gap: 2, marginLeft: 12 }}>
-        <NavLink href="/" label="New job" icon={<PlusIcon />} />
-        <NavLink href="/jobs" label="Job history" icon={<ClockIcon />} />
+        <NavLink href="/" label="New thread" icon={<PlusIcon />} />
+        <NavLink href="/jobs" label="Thread history" icon={<ClockIcon />} />
         <NavLink href="/metrics" label="Metrics" icon={<BarChartIcon />} />
         <NavLink href="/how-it-works" label="How it works" icon={<InfoIcon />} />
       </div>
